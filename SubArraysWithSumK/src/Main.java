55a48c518523765bc1b783dbbb772748d7d74e11
sanjay binnar <72009615+Binnar81@users.noreply.github.com>
2023-11-25 21:35:34 +0530

    Add files via upload

--- SubArraysWithSumK/src/Main.java
+++ SubArraysWithSumK/src/Main.java
@@ -1,23 +1,16 @@
 import java.util.*;
- class Solution {
-    public static List< List < Integer > > subarraysWithSumK(int []a, long k) {
-        // Write your code here
-
-
-        //Optimised
-        return optimised(a, k);
-    }
-
-    private static List<List<Integer>> naive(int[] a, long k) {
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < a.length; i++) {
-            long sum = 0;
-            ArrayList<Integer> sumList = new ArrayList<>();
-            for (int j = i; j < a.length; j++) {
-                sum += a[j];
-                sumList.add(a[j]);
-                if (sum == k) {
-                    ans.add(sumList);
+ class Solution{
+    public static List<List<Integer>> subarraysWithSumK(int[] a, long k) {
+        List<List<Integer>> ans=new ArrayList<>();
+        int n=a.length;
+        for(int i=0;i<n;i++){
+            long sum=0;
+            ArrayList<Integer> subarray=new ArrayList<>();
+            for(int j=i;j<n;j++){
+                sum+=a[j];
+                subarray.add(a[j]);
+                if(sum==k){
+                    ans.add(subarray);
                     break;
                 }
             }
@@ -25,26 +18,12 @@ import java.util.*;
         return ans;
     }
 
-    private static List<List<Integer>> optimised(int[] a, long k) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Map<Long, Integer> sumToIdx = new HashMap<>();
-        sumToIdx.put(0l, -1);
-        long sum = 0;
-        for (int i = 0; i < a.length; i++) {
-            sum += a[i];
-            if (sumToIdx.containsKey((sum-k))) {
-                ans.add(createSubarray(a, sumToIdx.get(sum-k)+1, i));
-            }
-            sumToIdx.put(sum, i);
-        }
+public static void main(String[] args){
+    int[] a={1,3,4,1,1,1};
+    List<List<Integer>> ans=subarraysWithSumK(a,3);
+    for(List<Integer> count:ans){
+        System.out.print(count+ ",");
+    }
 
-        return ans;
     }
-    private static List<Integer> createSubarray(int[] arr, int s, int e) {
-        List<Integer> ans = new ArrayList<>();
-        for (int i = s; i <= e; i++) {
-            ans.add(arr[i]);
-        }
-        return ans;
-    }
-}
+ }
